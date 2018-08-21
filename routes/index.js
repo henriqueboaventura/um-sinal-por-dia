c8d5ceb52000cb677defdc2b741f33587c209f6f
lucasborges <Lucas_Borges@gap.com>
2018-08-21 18:53:59 -0300

    adding a simple feature to click and brings other random word

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,13 @@ var request = require('request');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   signalId = Math.floor(Math.random() * (5823 - 1) + 1);
+  console.log("request: 'http://www.acessibilidadebrasil.org.br/libras_3/ajax/getWordById/" + signalId + "'");
   request('http://www.acessibilidadebrasil.org.br/libras_3/ajax/getWordById/' + signalId, function (error, response, body) {
     var content = JSON.parse(body);
     console.log(content.data)
     res.render(
-      'index', 
-      { 
+      'index',
+      {
         title: '1 sinal por dia!',
         word: content.data.palavra,
         description: content.data.acepcao,
@@ -18,8 +19,6 @@ router.get('/', function(req, res, next) {
       }
     );
   });
-
-  
 });
 
 module.exports = router;
